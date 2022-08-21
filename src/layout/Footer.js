8423eaaf0b4f998ccfc6d9a88f7ff6fab8863273
Tandrima <sarkartandrima123@gmail.com>
2022-08-21 21:27:07 +0530

    update footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,4 +1,88 @@
 import "./styles.css";
+
 export default function Footer() {
-  return <footer>I am footer</footer>;
+  return (
+    <footer>
+      <section class="footerbackdesign footerbackpadding">
+        <div class="container text-center text-md-start mt-5">
+          {/* <!-- Grid row  */}
+          <div class="row mt-3">
+            {/* Grid column --> */}
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              {/* Content --> */}
+              <p>
+                <span class="clickable cursor">Audio Description</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Investor Relations</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Legal Notices</span>
+              </p>
+            </div>
+            {/* Grid column --> */}
+
+            {/* Grid column --> */}
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              {/* Links --> */}
+
+              <p>
+                <span
+                  class="
+                clickable cursor"
+                >
+                  Help Centre
+                </span>
+              </p>
+              <p>
+                <span class="clickable cursor">Jobs</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Cookie Preferences</span>
+              </p>
+            </div>
+            {/* Grid column --> */}
+
+            {/* Grid column --> */}
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              {/* Links --> */}
+
+              <p>
+                <span class="clickable cursor">Gift Cards</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Terms of Use</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Corporate Information</span>
+              </p>
+            </div>
+            {/* Grid column --> */}
+
+            {/* Grid column --> */}
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+              {/* Links --> */}
+
+              <p>
+                <span class="clickable cursor">Media Centre</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Privacy</span>
+              </p>
+              <p>
+                <span class="clickable cursor">Contact Us</span>
+              </p>
+            </div>
+            {/* Grid column --> */}
+          </div>
+          {/* Grid row --> */}
+        </div>
+      </section>
+      {/* Section: Links  --> */}
+
+      {/* Copyright --> */}
+      <div class="footerbackdesign copyright">© 1997-2022 Netflix, Inc.</div>
+      {/* {/* Copyright  */}
+    </footer>
+  );
 }
