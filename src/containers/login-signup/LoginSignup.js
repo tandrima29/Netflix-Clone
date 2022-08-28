3e822ffd8d4a7618b8b34b0a3b2ea95f4247fafc
narayan25-pixel <55695796+narayan25-pixel@users.noreply.github.com>
2022-08-28 22:04:59 +0530

    header

--- src/containers/login-signup/LoginSignup.js
+++ src/containers/login-signup/LoginSignup.js
@@ -1,7 +1,12 @@
+import Button from "../../components/button/Button";
+import Header from "../../layout/Header";
+import Hero from "../../components/hero-section/hero";
+
 export default function LoginSignup() {
-    return (
-        <div>
-            I am LS
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
 }
