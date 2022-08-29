292b623d895b437a8e043f7f0aa895bc788eaa1c
Tandrima <sarkartandrima123@gmail.com>
2022-08-29 22:09:58 +0530

    fixed login

--- src/containers/login-signup/LoginSignup.js
+++ src/containers/login-signup/LoginSignup.js
@@ -1,7 +1,49 @@
+import "./loginpage.css";
 export default function LoginSignup() {
-    return (
-        <div>
-            I am LS
-        </div>
-    )
+  return (
+    <div className=" container-fluid login-body">
+      <div className="login">
+        <h1>Sign In</h1>
+        <form className="form-margin">
+          <div className="form-floating form-margin bg-secondary">
+            <input
+              type="Email"
+              className="form-control "
+              id="floatingEmail"
+              placeholder="email"
+            />
+            <label for="floatingPassword">Email or phone number</label>
+          </div>
+
+          <div className="form-floating form-margin bg-dark">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <button
+            type="submit"
+            className="btn bg-danger w-100 form-margin text-white"
+          >
+            Sign In
+          </button>
+          {/* <div className="row mt-2">
+            <div className="col-6">
+              <label>
+                <input type="checkbox" />
+                <span className="checkmark"></span>
+                <span style="font-size: 0.7rem">Remember me</span>
+              </label>
+            </div>
+            <div className="col-6">
+              <span className="forgotpassword">Need Help?</span>
+            </div> */}
+        </form>
+      </div>
+    </div>
+  );
 }
