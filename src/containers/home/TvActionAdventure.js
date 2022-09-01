6e79b28a0d96ae2f1d369a7b1a269118a9834eba
tandrima29 <107115320+tandrima29@users.noreply.github.com>
2022-09-01 22:46:27 +0530

    generes are added (#6)
    diffenent section

---
+++ src/containers/home/TvActionAdventure.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import MovieCard from "../../components/movie-card/MovieCard";
+import axios from "axios";
+
+export default function TvActionAdventure() {
+  const [recentWatch, updateRecentWatch] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://630722593a2114bac75a5755.mockapi.io/netflix-clone/top-10-to-watch"
+      )
+      .then((response) => {
+        const result = response.data.content;
+
+        updateRecentWatch(result);
+      })
+      .catch((error) => {});
+  }, []);
+  return (
+    <div className="container-fluid">
+      <h5 className="text-white">TV Actions & Adventures</h5>
+      <div className="row slider-wrapper">
+        {recentWatch.map((item, index) => {
+          return (
+            <div className="col-sm-2 my-2 clickable">
+              <MovieCard
+                bgImg={item.imgUrl}
+                isNetflixOrigin={item.isNetflixOrigin}
+                isTopTen={item.isTopTen}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
